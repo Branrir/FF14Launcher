e443b17b43907cda766e3945ac3cd88f441abd20
Alexey Sorokin <alexey.sorokin.work@gmail.com>
2019-07-02 15:45:11 +0200

    update

--- launcher.py
+++ launcher.py
@@ -9,22 +9,21 @@
 from getpass import getpass
 from login import *
 import subprocess
-# Needed to handle the config file
+#Needed to handle the config file
 import os
 import sys
-if (sys.version_info >= (3, 0)):
+if (sys.version_info >= (3,0)):
 	from configparser import ConfigParser
 else:
 	from ConfigParser import ConfigParser
 
-
 def gen_launcher_command(settings):
-	exe_path = settings['path']
+	exe_path=settings['path']
 
 	if(settings['use_dx11'].strip() == 'True'):
-		exe_path = join_path(exe_path, 'game/ffxiv_dx11.exe')
+		exe_path = join_path(exe_path,'game/ffxiv_dx11.exe')
 	else:
-		exe_path = join_path(exe_path, 'game/ffxiv.exe')
+		exe_path = join_path(exe_path,'game/ffxiv.exe')
 
 	launcher_dict = [
 		exe_path,
@@ -37,24 +36,23 @@ def gen_launcher_command(settings):
 		'SYS.Region='+settings['region'],
 		'ver='+settings['version']]
 
-	# Deal with pre_command (NOT Running on windows)
+	#Deal with pre_command (NOT Running on windows)
 	if settings['pre_command'].strip() != '':
-		launcher_dict.insert(0, settings['pre_command'].strip())
+		launcher_dict.insert(0,settings['pre_command'].strip())
 
 	return launcher_dict
 
 def run(settings):
-	sid = login(settings['region'], settings['user'], settings['password'], settings['one_time_password'])
-	(settings['actual_sid'], settings['version']) = get_actual_sid(sid, settings['path'])
+	sid=login(settings['region'],settings['user'],settings['password'],settings['one_time_password'])
+	(settings['actual_sid'],settings['version']) = get_actual_sid(sid,settings['path'])
 	launch = gen_launcher_command(settings)
 	for i in launch:
-		print(i, end=' ')
+		print(i,end=' ')
 	subprocess.run(launch)
-
 
 def run_cli(settings):
 	if (settings['user'] == ''):
-		settings['user'] = raw_input("User Name:  ")
+		settings['user'] = input("User Name:  ")
 	if (settings['password'] == ''):
 		settings['password'] = getpass()
 	try:
@@ -62,21 +60,20 @@ def run_cli(settings):
 	except Exception as err:
 		print("Error:  " + str(err))
 
-
 class gui_prompt:
 	def run_gui(self):
-		# Store the user input
+		#Store the user input
 		settings['user']=self.E1.get();
-		settings['password'] = self.E2.get()
-		settings['one_time_password'] = self.E3.get()
-		# Disable the GUI
+		settings['password']=self.E2.get();
+		settings['one_time_password']=self.E3.get()
+		#Disable the GUI
 		self.top.quit()
 		self.top.destroy()
-		# Run the Program
+		#Run the Program
 		try:
 			run(settings)
 		except Exception as err:
-			if (sys.version_info >= (3, 0)):
+			if (sys.version_info >= (3,0)):
 				import tkinter
 				from tkinter.messagebox import showwarning
 			else:
@@ -87,7 +84,7 @@ class gui_prompt:
 			showwarning("Error", str(err), parent=top)
 
 	def __init__(self,settings):
-		if (sys.version_info >= (3, 0)):
+		if (sys.version_info >= (3,0)):
 			import tkinter
 		else:
 			import Tkinter as tkinter
@@ -98,31 +95,32 @@ class gui_prompt:
 		self.E1 = tkinter.Entry(self.top, textvariable=tkinter.StringVar(value=settings['user']))
 		self.E1.grid(row = 0, column = 1)
 		self.L2 = tkinter.Label(self.top, text="Password")
-		self.L2.grid(row=1, column=0)
+		self.L2.grid(row = 1, column = 0)
 		self.E2 = tkinter.Entry(self.top, show="*", textvariable=tkinter.StringVar(value=settings['password']))
-		self.E2.grid(row=1, column=1)
+		self.E2.grid(row = 1, column = 1)
 		self.L3 = tkinter.Label(self.top, text="One Time Password")
-		self.L3.grid(row=2, column=0)
+		self.L3.grid(row = 2, column = 0)
 		self.E3 = tkinter.Entry(self.top, textvariable=tkinter.StringVar(value=settings['one_time_password']))
 		self.E3.grid(row = 2, column = 1)
 
-		self.OK = tkinter.Button(self.top, text="Connect", command=self.run_gui)
-		self.OK.grid(row=3, column=1)
+		self.OK = tkinter.Button(self.top, text ="Connect", command = self.run_gui)
+		self.OK.grid(row = 3, column = 1)
 		self.top.bind('<Return>', lambda _: self.OK.invoke())
 		self.top.bind('<KP_Enter>', lambda _: self.OK.invoke())
 
-		# Place window in center of screen
+		#Place window in center of screen
 		self.top.eval('tk::PlaceWindow %s center' % self.top.winfo_pathname(self.top.winfo_id()))
-		# Focus on the one time password box at start
+		#Focus on the one time password box at start
 		self.E3.focus()
 		self.top.title("FFXIV Launcher")
 		self.top.mainloop()
 
 
-# Read config file from launcher directory
-config_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+#Read config file from launcher directory
+config_path=os.path.dirname(os.path.realpath(sys.argv[0]))
 config = ConfigParser()
-config.read(join_path(config_path, 'launcher_config.ini'))
+config.read(join_path(config_path,'launcher_config.ini'))
 settings = config._sections['FFXIV']
 
 if len(sys.argv) > 1:
